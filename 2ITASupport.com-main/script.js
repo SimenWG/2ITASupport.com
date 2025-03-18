fb5b3b8aa22f936af3456cd5060f88d8f7455561
Simen W <148765568+SimenWG@users.noreply.github.com>
2025-03-18 07:55:33 +0100

    fikset utseende

---
+++ 2ITASupport.com-main/script.js
@@ -0,0 +1,37 @@
+let scrollBtn = document.getElementById('scrollUp');
+
+window.onscroll = function () {
+    scrollFunction()    
+}
+
+function scrollFunction() {
+    if (document.body.scrollTop > 590 || document.documentElement.scrollTop > 590) {
+        scrollBtn.style.display = "block";
+    } else {
+        scrollBtn.style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+// hamburger meny
+const hamMenu = document.getElementById('hamMenu');
+const navLinks = document.querySelector('.nav-links');
+
+hamMenu.addEventListener("click", function () {
+    navLinks.classList.toggle('active');
+});
+
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+}
+
+function windowLocation() {
+    window.location.href = "../index.html"
+}
